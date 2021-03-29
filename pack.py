baabccdbb81fbb5be05cc9b05e8a5c9528dc9ac7
asdf5283 <just5283@gmail.com>
2021-03-29 16:26:27 +0900

    팩맨 만들기

---
+++ pack.py
@@ -0,0 +1,11 @@
+print("팩맨 게임을 시작합니다.")
+print("유령이 나타났다")
+print("1.도망간다 2.아이템을 쓴다 3.싸운다")
+number = int(input("숫자를 입력하세요 : "))
+print("유저 입력값 : ", number)
+if number == 1:
+    print("도망에 성공했다!")
+if number == 2:
+    print("아이템을 사용했다")
+if number == 3:
+    print("싸운다")
---
+++ pokemon.py
@@ -0,0 +1,14 @@
+print ("    포켓몬스터 RPG")
+print ("1.이상해씨 2.파이리 3.꼬북이")
+start_pk = input("스타팅 포켓몬을 선택해주세요 : ")
+
+if '1' in start_pk:
+    print ("이상해씨를 선택하셨습니다.")
+elif '2' in start_pk:
+    print ("파이리를 선택하셨습니다.")
+elif '3' in start_pk:
+    print ("꼬북이를 선택하셨습니다.")
+else:
+    print ("포켓몬을 다시 선택해주세요.")
+print (".\n.\n.\n")
+print ("이제 모험을 시작합니다!")
